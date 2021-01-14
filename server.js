89fe1895030f77d131b1a58a6a8635ee90a61099
Adebambo Adeshiyan <badeshiyan@gmail.com>
2021-01-13 21:42:51 -0500

    deploying

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+var PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 const app = express();
 
@@ -14,11 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
